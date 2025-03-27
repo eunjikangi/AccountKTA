19bc3cb1ae63e54c14ce9dbbefb5d53a1f7c9b89
eunjikangi <miki2323@naver.com>
2025-03-27 12:54:57 +0900

    Account TDD

--- GTestTemplate3/account.cpp
+++ GTestTemplate3/account.cpp
@@ -6,6 +6,36 @@ public:
 		return balance;
 	}
 
+	void deposit(int money) {
+		balance += money;
+	}
+
+	void withdraw(int money) {
+		balance -= money;
+	}
+
+	int getBalanceApplyInterest(int bal, int rate) {
+		return bal * rate * 0.01;
+	}
+
+	void updateInterest() {
+		balance += getBalanceApplyInterest(balance, rate);
+	}
+
+	void setInterest(int rate) {
+		this->rate = rate;
+	}
+
+	int getPredictInterest(int year) {
+		int predictBalance = balance;
+
+		for (int i = 0; i < year; i++) {
+			predictBalance += getBalanceApplyInterest(predictBalance, rate);
+		}
+
+		return predictBalance;
+	}
 private:
 	int balance;
+	int rate = {5};
 };
--- GTestTemplate3/main.cpp
+++ GTestTemplate3/main.cpp
@@ -1,10 +1,50 @@
 #include "gmock/gmock.h"
+#include "account.cpp"
 
 using namespace std;
 
-TEST(AccountTest, CreateAccountInit10000won)
-{
+class AccountFixture : public testing::Test {
+public:
+	Account account{ 10000 };
+};
 
+
+TEST_F(AccountFixture, CreateAccountInit10000won)
+{
+	EXPECT_EQ(10000, account.getBalace());
+}
+
+TEST_F(AccountFixture, Deposit)
+{
+	account.deposit(500);
+	EXPECT_EQ(10500, account.getBalace());
+}
+
+TEST_F(AccountFixture, Withdraw)
+{
+	account.withdraw(600);
+	EXPECT_EQ(9400, account.getBalace());
+}
+
+TEST_F(AccountFixture, FivePercentInterest)
+{
+	account.updateInterest();
+	EXPECT_EQ(10500, account.getBalace());
+}
+
+TEST_F(AccountFixture, TenPercentInterest)
+{
+	account.setInterest(10);
+	account.updateInterest();
+	EXPECT_EQ(11000, account.getBalace());
+}
+
+TEST_F(AccountFixture, InterestAfterNyear)
+{
+	account.setInterest(10);
+	int ret = account.getPredictInterest(3);
+	int expected = 13310;
+	EXPECT_EQ(expected, ret);
 }
 
 int main() {
